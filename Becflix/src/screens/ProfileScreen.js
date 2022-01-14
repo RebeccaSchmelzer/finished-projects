902b4370418653185bdb20058f61602ac1f7900c
Rebecca Schmelzer <57275500+RebeccaSchmelzer@users.noreply.github.com>
2022-01-14 11:10:21 -0500

    Add files via upload

---
+++ Becflix/src/screens/ProfileScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './ProfileScreen.css'
+
+function ProfileScreen() {
+    return (
+        <div className='profileScreen'>
+            <h1>
+                hello
+            </h1>
+        </div>
+    )
+}
+console.log("hello")
+
+export default ProfileScreen
